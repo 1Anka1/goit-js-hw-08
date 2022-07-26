7fc93750a58615094700f5412362fb3465bcb322
Your Name <97912793+1Anka1@users.noreply.github.com>
2022-07-26 21:25:32 +0200

    Add Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join("");
+}
+galleryList.insertAdjacentHTML(
+    "beforeend",
+    createGalleryCardsMarkup(galleryItems)
+  );
+
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
