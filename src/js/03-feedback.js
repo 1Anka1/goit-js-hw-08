7c8caf86d71b467fe2304b4e8ccc434bd39cb301
Your Name <97912793+1Anka1@users.noreply.github.com>
2022-07-28 19:17:46 +0200

    Update Task 3
    Add validation to the form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,11 +2,11 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const refs ={
-    form: document.querySelector('.feedback-form'),
-    message : document.querySelector('.feedback-form textarea'),
-    email : document.querySelector('.feedback-form input'),
-}
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  message: document.querySelector('.feedback-form textarea'),
+  email: document.querySelector('.feedback-form input'),
+};
 
 refs.form.addEventListener('input', throttle(onFormData, 500));
 refs.form.addEventListener('submit', onFormSubmit);
@@ -15,22 +15,28 @@ const formData = {};
 
 dataFromLocalStorage();
 
-function onFormData(e){
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function onFormSubmit (e){
-    e.preventDefault();
-    e.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  if (refs.email.value === '' || refs.message.value === '') {
+    return alert ('Заповни будь-ласка поля!');
+  }
+  formData.email = refs.email.value;
+  formData.message = refs.message.value;
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function dataFromLocalStorage() {
-    const data =  JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if(data){
-        refs.email.value = data.email;
-        refs.message.value = data.message;
-        console.log(data);
-    }
-  };
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (data) {
+    refs.email.value = data.email;
+    refs.message.value = data.message;
+    console.log(data);
+  }
+}
